3a2257dc80c1853ead5e14e60e2f4b41a0ebed63
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-06-08 10:09:47 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_AbelPerez/DswiiCl1SoapAbelPerezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_AbelPerez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapAbelPerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapAbelPerezApplication.class, args);
+	}
+
+}
